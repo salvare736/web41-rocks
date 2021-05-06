2ba6b1d567338cc79f99460c30375b61f913d460
Sergio Alvarez <salvare736@gmail.com>
2021-05-06 13:36:21 -0400

    cleaned up the examples a bit

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 console.log('Hello!');
+
 require('dotenv').config();
+
 const express = require('express');
 
 const server = express();
 
 // this line works from the start
 const PORT = process.env.PORT || 5000;
+
+// using 'dotenv' we can process local environment variables on our local machine only (because of gitignore)
+// process.env.API_KEY
 
 server.use(express.json());
 
