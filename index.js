674336d8a531471df627df2752b6b30b9b909669
Sergio Alvarez <salvare736@gmail.com>
2021-05-06 13:57:02 -0400

    trying to add react frontend

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ console.log('Hello!');
 
 require('dotenv').config();
 
+const path = require('path');
+
 const express = require('express');
 
 const server = express();
@@ -12,12 +14,18 @@ const PORT = process.env.PORT || 5000;
 // using 'dotenv' we can process local environment variables on our local machine only (because of gitignore)
 // process.env.API_KEY
 
+server.use(express.static(path.join(__dirname, 'client/build')));
+
 server.use(express.json());
 
 server.get('/api/*', (req, res) => {
     res.json({
         cohort: 'web 41'
     });
+});
+
+server.use('*', (_, res) => {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
 
 server.listen(PORT, () => {
