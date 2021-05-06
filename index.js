92f513f0944beba579a49ccdc89d93439918146b
Sergio Alvarez <salvare736@gmail.com>
2021-05-06 12:48:44 -0400

    Initial setup

---
+++ index.js
@@ -0,0 +1,18 @@
+console.log('Hello!');
+const express = require('express');
+
+const server = express();
+
+const PORT = process.env.PORT || 5000;
+
+server.use(express.json());
+
+server.get('/api/*', (req, res) => {
+    res.json({
+        cohort: 'web 41'
+    });
+});
+
+server.listen(PORT, () => {
+    console.log(`listening on ${PORT}`);
+});
